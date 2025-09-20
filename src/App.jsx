b4049ac6ec1731ec4a768145253f8e6a0a718270
Jonathan Longman <jonathan.longman@live.com.au>
2025-09-20 14:51:29 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello, World! 👋</h1>
+      <p>This is your first React app running on Windows!</p>
+    </div>
+  );
+}
+
+export default App;
